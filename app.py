e8f5af73aa5e7f51acc651895c12ffbe49f35043
smartsingh <smartsingh@gmail.com>
2018-12-18 21:08:40 -0500

    added second route with user filter

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ from flask import (
     request,
     redirect)
 
+from datetime import date
+from datetime import time
+from datetime import datetime
+
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
@@ -47,6 +51,34 @@ def home():
 def json():
     """Return a list of accidents"""
     jAccidents = session.query(Accidents).all()
+
+    # Create a dictionary from the row data and append to a list of all accidents
+    accidents = []
+    for accident in jAccidents:
+        accident_dict = {}
+        accident_dict["apc"] = accident.AirportCode
+        accident_dict["apn"] = accident.AirportName
+        accident_dict["loc"] = accident.Location
+        accident_dict["con"] = accident.Country
+        accident_dict["bpf"] = accident.BroadPhaseOfFlight
+        accident_dict["inj"] = accident.InjurySeverity
+        accident_dict["cat"] = accident.AircraftCategory
+        accident_dict["wec"] = accident.WeatherCondition
+        accident_dict["slat"] = accident.start_lats
+        accident_dict["slon"] = accident.start_lons
+        accident_dict["flat"] = accident.Latitude
+        accident_dict["flon"] = accident.Longitude
+        accidents.append(accident_dict)
+
+    return jsonify(accidents)
+
+@app.route("/accident_filter/<user_filter>")
+def accident_filter(user_filter):
+    """Return a list of accidents"""
+    jAccidents = session.query(Accidents).filter(Accidents.EventDate.strftime("%Y") == user_filter["selYear"])\
+                                         .filter(Accidents.BroadPhaseOfFlight == user_filter["selBPF"])\
+                                         .filter(Accidents.AircraftCategory == user_filter["selACC"])\
+                                         .filter(Accidents.InjurySeverity == user_filter["selIS"]).all()
 
     # Create a dictionary from the row data and append to a list of all accidents
     accidents = []
